91fc5d0983c8dc3fbd5b19e4ed2fad1f367661bf
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-06-25 15:23:17 +0700

    add

--- apps/toko/next.config.js
+++ apps/toko/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -10,6 +11,25 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  webpack: (config) => {
+    config.resolve.alias['@tainext/ui'] = path.resolve(
+      __dirname,
+      '../../libs/ui/src'
+    );
+    config.resolve.alias['@tainext/utils'] = path.resolve(
+      __dirname,
+      '../../libs/utils/src'
+    );
+    config.resolve.alias['@tainext/db'] = path.resolve(
+      __dirname,
+      '../../libs/db/src'
+    );
+    config.resolve.alias['@tainext/auth'] = path.resolve(
+      __dirname,
+      '../../libs/auth/src'
+    );
+    return config;
+  },
 };
 
 const plugins = [
